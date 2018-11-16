ca35a6e5696f88e080e079a2b7503e1bc1e25f96
salt0916 <nozawas@cotegg.com>
2018-11-16 06:33:03 +0000

    Finish layout and routes

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -6,43 +6,43 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get microposts_url
+    get microposts
     assert_response :success
   end
 
   test "should get new" do
-    get new_micropost_url
+    get new_microposts
     assert_response :success
   end
 
   test "should create micropost" do
     assert_difference('Micropost.count') do
-      post microposts_url, params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
+      post microposts, params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
     end
 
     assert_redirected_to micropost_url(Micropost.last)
   end
 
   test "should show micropost" do
-    get micropost_url(@micropost)
+    get microposts(@micropost)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_micropost_url(@micropost)
+    get edit_microposts(@micropost)
     assert_response :success
   end
 
   test "should update micropost" do
-    patch micropost_url(@micropost), params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
+    patch microposts(@micropost), params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
     assert_redirected_to micropost_url(@micropost)
   end
 
   test "should destroy micropost" do
     assert_difference('Micropost.count', -1) do
-      delete micropost_url(@micropost)
+      delete microposts(@micropost)
     end
 
-    assert_redirected_to microposts_url
+    assert_redirected_to micropostss
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,20 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # Setup all fixtures in test/fixtures/*.yml for all tests
+  # in alphabetical order.
   fixtures :all
-
+  include ApplicationHelper
   # Add more helper methods to be used by all tests here...
+end
+  
+class ApplicationHelperTest < ActionView::TestCase
+test "full title helper" do
+  assert_equal full_title,         FILL_IN
+  assert_equal full_title("Help"), FILL_IN
+end
 end
