07fea956edd786b1e1c48def1affd39e98b89e55
salt0916 <nozawas@cotegg.com>
2018-11-19 04:33:07 +0000

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,16 +5,9 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests
-  # in alphabetical order.
   fixtures :all
-  include ApplicationHelper
-  # Add more helper methods to be used by all tests here...
-end
   
-class ApplicationHelperTest < ActionView::TestCase
-test "full title helper" do
-  assert_equal full_title,         FILL_IN
-  assert_equal full_title("Help"), FILL_IN
-end
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
